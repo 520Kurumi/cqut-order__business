16b025c0ce716e2b5c4a0a69274b58a759818ab9
520Kurumi <2356156537@qq.com>
2023-05-05 22:18:28 +0800

    feat: configure vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', //解决控制台 ：Network: use --host to expose
+    port: 8080, //配置端口号
+    hmr: true, //开启热更新
+    open: true //启动在浏览器打开
+    }    
 })
