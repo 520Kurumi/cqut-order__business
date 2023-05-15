1c0a4fc736966198258b3a9394d3e8238cd7f699
520Kurumi <2356156537@qq.com>
2023-05-15 16:43:46 +0800

    feat: 一些环境配置，如：router、pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+//import './style.css'
 import App from './App.vue'
+//createApp(App).mount('#app')
+//引入element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 
-createApp(App).mount('#app')
+import router from './router/index'
+const app = createApp(App);
+app.use(ElementPlus).use(router).use(pinia).mount('#app')
+
+//全局注册图标组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -9,5 +9,13 @@ export default defineConfig({
     port: 8080, //配置端口号
     hmr: true, //开启热更新
     open: true //启动在浏览器打开
-    }    
+    },
+    resolve: {
+      alias: [
+        {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+        }
+      ]
+      }    
 })
